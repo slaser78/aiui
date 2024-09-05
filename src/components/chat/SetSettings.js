a84ac1bcf3a55e5a0660d0bfe7d776b468b7174c
scott laser <scott.l.laser.ctr@mail.mil>
2024-09-05 14:32:16 -0400

    Initial

--- src/components/chat/SetSettings.js
+++ src/components/chat/SetSettings.js
@@ -9,6 +9,8 @@ import DialogTitle from '@mui/material/DialogTitle';
 import useAPI from "../../useAPI";
 import Slider from '@mui/material/Slider';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 const options = {
     headers: {
@@ -19,18 +21,55 @@ const options = {
 };
 const person = localStorage.getItem('username');
 
-const SetSettings = (props) => {
+const GetSources = (props) => {
     const urlValue = React.useContext(UrlContext);
-    const [accuracyValue, setAccuracyValue] = React.useState(null);
-    const entry = useAPI(urlValue.urlValue + `/getChatSettings?person=${person}`)
-
+    const [sources, setSources] = React.useState([]);
+    const entry = useAPI(urlValue.urlValue + `/getSources?person=${person}`)
     React.useEffect(() => {
-        if (entry) {
-            setAccuracyValue(entry.accuracy)
-        }
+        setSources(entry)
         //eslint-disable-next-line
     }, [entry]);
+    return (
+        sources
+    )
+}
 
+const GetSource = (props) => {
+    const urlValue = React.useContext(UrlContext);
+    const [source, setSource] = React.useState([]);
+    const entry = useAPI(urlValue.urlValue + `/getSource?person=${person}`)
+    React.useEffect(() => {
+        setSource(entry)
+        //eslint-disable-next-line
+    }, [entry]);
+    return (
+        source
+    )
+}
+
+const SetSettings = (props) => {
+    const urlValue = React.useContext(UrlContext);
+    const [accuracyValue, setAccuracyValue] = React.useState(0.1);
+    const [source, setSource] = React.useState([]);
+    const [sources, setSources] = React.useState([]);
+    const entry = useAPI(urlValue.urlValue + `/getChatSettings?person=${person}`)
+    const fetchedSources = GetSources();
+    const [personSource, setPersonSource] = React.useState(null);
+    const fetchedSource = GetSource();
+    React.useEffect(() => {
+        // set 'source' state with fetchedSources
+        setSource(fetchedSources);
+        setPersonSource(fetchedSource);
+    }, [fetchedSources, fetchedSource]);
+
+    React.useEffect(() => {
+        if (entry && source && personSource) {
+            setAccuracyValue(entry.accuracy ? entry.accuracy : 0.1)
+            setSources(source)
+            setPersonSource(personSource)
+        }
+        //eslint-disable-next-line
+    }, [entry,source, personSource]);
 
     const cancel = () => {
         props.setSettingsOpen(false)
@@ -39,6 +78,12 @@ const SetSettings = (props) => {
     const handleChange = (event, newValue) => {
         setAccuracyValue(newValue);
     };
+
+    const fetchedSource2 = GetSource();
+    const sourceIndexes = sources.map((source, index) => {
+        return fetchedSource2.findIndex(fetchedItem => fetchedItem.id === source.id) !== -1 ? index : -1;
+    }).filter(index => index !== -1);
+    console.log ("Source Indexes: " + sourceIndexes.toString());
 
     return (
         <div>
@@ -53,7 +98,7 @@ const SetSettings = (props) => {
                         onSubmit={ async values  => {
                             props.setSettingsOpen(false);
                             const person = localStorage.getItem("username")
-                            await axios.post(urlValue.urlValue + `/setChatSettings?accuracy=${accuracyValue}&person=${person}`, options)
+                            await axios.post(urlValue.urlValue + `/setChatSettings?accuracy=${accuracyValue}&person=${person}&sources=${personSource}`, options)
                                 .then((response) => {
                                     props.setData([...props.data, response.data])
                                 }, (error) => {
@@ -66,21 +111,39 @@ const SetSettings = (props) => {
                                 <div className='row'>
                                     <div className="form-group">
                                         <ul><label className="control-label">Accuracy: </label></ul>
-                                        <ul><Box sx={{ width: 200 }}>
-                                            <Slider
-                                                valueLabelDisplay="auto"
-                                                shiftStep={0.7}
-                                                step={0.1}
-                                                marks
-                                                min={0.1}
-                                                max={1.0}
-                                                value={accuracyValue} onChange={handleChange} />
-                                        </Box>
+                                        <ul>
+                                            <Box sx={{ width: 200 }}>
+                                                <Slider
+                                                    valueLabelDisplay="auto"
+                                                    shiftStep={0.7}
+                                                    step={0.1}
+                                                    marks
+                                                    min={0.1}
+                                                    max={1.0}
+                                                    value={accuracyValue} onChange={handleChange} />
+                                            </Box>
                                         </ul>
                                     </div>
                                 </div>
-                                <br/>
-                                <span><input className="btn-info m-2 p-2" type="submit"/>&nbsp;&nbsp;&nbsp;
+                                <div className='row'>
+                                    <div className="form-group">
+                                        <ul>
+                                            <Autocomplete
+                                                multiple
+                                                options={sources}
+                                                sx={{width: 300}}
+                                                filterSelectedOptions
+                                                value={sources?.slice(sourceIndexes.toString())  || []}
+                                                onChange={(event, newValue) => {
+                                                    setPersonSource(newValue);  // Control the state based on changes
+                                                }}
+                                                renderInput={(params) => <TextField {...params} label="Sources"/>}
+                                            />
+                                        </ul>
+                                    </div>
+                                </div>
+                            <br/>
+                            <span><input className="btn-info m-2 p-2" type="submit"/>&nbsp;&nbsp;&nbsp;
                                     <button className="btn-danger m-2 p-2" type='button'
                                             onClick={cancel}>Cancel</button>
                                     </span>
